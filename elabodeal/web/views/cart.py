3f038fb05c51e4d53e78f39c401bb5c93f1b15b9
Maklero <michalgolda@vp.pl>
2020-07-08 15:50:46 +0200

    Add product to cart and delete function

--- elabodeal/web/views/cart.py
+++ elabodeal/web/views/cart.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import redirect
 from django.template.loader import render_to_string
 
 from elabodeal.models import Product
@@ -9,10 +10,67 @@ class CartView(View):
 	def respond_cart(self, request, context = None):
 		return HttpResponse(render_to_string('cart.html', context, request))
 
-	def get(self, request):
-		products = Product.objects.all()
+	def get_cart_products(self, request):
+		products = []
+		for p in request.session['cart']['products']:
+			product = Product.objects.filter(id=p['id']).first()
+			products.append(product)
 
+		return products
+
+	def calculate_total_price(self, request):
+		total_price = 0.00
+
+		for p in request.session['cart']['products']:
+			total_price += p['price']
+
+		total_price = '{0:.2f}'.format(round(total_price, 2))
+
+		return total_price
+
+	def post(self, request):
+		if not 'cart' in request.session:
+			request.session['cart'] = {
+				'products': [],
+				'total_price': 0.00
+			}
+
+		action_type = request.POST.get('action_type')
+		
+		if action_type == 'add-product-to-cart':
+			product_id = int(request.POST.get('product_id'))
+			product_price = float(request.POST.get('product_price'))
+
+			cart = request.session['cart']
+
+			cart['products'].append({'id': product_id, 'price': product_price})
+			cart['total_price'] = self.calculate_total_price(request)
+
+			request.session['cart'] = cart
+
+			context = {
+				'show_info': True,
+				'products': self.get_cart_products(request)
+			}
+			return self.respond_cart(request, context)
+
+		elif action_type == 'delete-product-from-cart':
+			product_id = int(request.POST.get('product_id'))
+
+			cart = request.session['cart']
+
+			for p in cart['products']:
+				if p['id'] == product_id:
+					cart['products'].remove(p)
+
+			cart['total_price'] = self.calculate_total_price(request)
+
+			request.session['cart'] = cart
+
+			return redirect('web:cart')
+
+	def get(self, request):
 		context = {
-			'products': [products[0]]
+			'products': self.get_cart_products(request)
 		}
 		return self.respond_cart(request, context)
