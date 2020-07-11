f5b24cbba5ad744f0cf35a35e0c6f3b8c6e3c319
Maklero <michalgolda@vp.pl>
2020-07-11 21:25:02 +0200

    Add salesmanager index page and page for add products

--- elabodeal/web/views/salesmanager.py
+++ elabodeal/web/views/salesmanager.py
@@ -1,9 +1,16 @@
 from elabodeal.web.views.base import BaseView
 
+from elabodeal.models import Product
+
 
 class SalesManagerView(BaseView):
 	auth_required = True
 	seller_required = True
 
 	def get(self, request):
-		return self.respond('salesmanager/index.html', request)
+		products = Product.objects.filter(author__username=request.user.username).all()
+
+		context = {
+			'products': products if len(products) > 0 else False
+		}
+		return self.respond('salesmanager/index.html', request, context)
--- tests/web/views/test_salesmanager.py
+++ tests/web/views/test_salesmanager.py
@@ -1,11 +1,37 @@
 from tests.base import WebTestCase
 from django.urls import reverse
 
-from elabodeal.models import User
+from elabodeal.models import User, Category, Product
 
 
 class TestSalesManagerView(WebTestCase):
 	def test_simple(self):
+		user = User.objects.create_user(email='test@test.pl', username='test', password='123')
+		user.is_seller = True
+		user.save()
+
+		category = Category()
+		category.name = 'Test'
+		category.save()
+
+		product = Product()
+		product.category = category
+		product.author = user
+		product.title = 'Test e-book'
+		product.description = 'test'
+		product.price = 12
+		product.save()
+
+		self.client.login(email='test@test.pl', password='123')
+
+		response = self.client.get(reverse('web:salesmanager'))
+
+		self.assertEqual(response._headers['content-type'][1], 'text/html; charset=utf-8')
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'salesmanager/index.html')
+		self.assertIn('Test e-book'.encode('utf-8'), response.content)
+
+	def test_when_user_is_not_has_products(self):
 		user = User.objects.create_user(email='test@test.pl', username='test', password='123')
 		user.is_seller = True
 		user.save()
@@ -17,6 +43,7 @@ class TestSalesManagerView(WebTestCase):
 		self.assertEqual(response._headers['content-type'][1], 'text/html; charset=utf-8')
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'salesmanager/index.html')
+		self.assertIn('Nie posiadasz żadnego opublikowanego e-booka zmień to jak najszybciej'.encode('utf-8'), response.content)
 
 	def test_when_user_is_not_seller(self):
 		user = User.objects.create_user(email='test@test.pl', username='test', password='123')
