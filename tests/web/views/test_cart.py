b2554e8bc417057bc48bd96ce631e0cfeb6ba4c0
Maklero <michalgolda@vp.pl>
2020-07-21 21:58:07 +0200

    Add test for check if product does exist in cart

--- tests/web/views/test_cart.py
+++ tests/web/views/test_cart.py
@@ -10,3 +10,18 @@ class TestCartView(WebTestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'cart.html')
 		self.assertIn('Twój koszyk jest pusty'.encode('utf-8'), response.content)
+
+	def test_redirect_if_product_does_exist_in_cart(self):
+		session = self.client.session
+		session['cart'] = {'products': [{'id': 1}]}
+		session.save()
+
+		form_data = {
+			'action_type': 'add-product',
+			'product_id': 1
+		}
+
+		response = self.client.post(reverse('web:cart'), form_data)
+		
+		self.assertEqual(response._headers['content-type'][1], 'text/html; charset=utf-8')
+		self.assertEqual(response.status_code, 302)
