a2809f47cacb70c88fec5a7931ca8cffa7534bca
Maklero <michalgolda@vp.pl>
2020-07-23 14:37:30 +0200

    Revert "Add send email for user which buying products and add products for user which has account"
    This reverts commit 3aee7e3f624d818fd72c021507ce4383c6b940bf.

--- elabodeal/api/views/payment.py
+++ elabodeal/api/views/payment.py
@@ -1,4 +1,3 @@
-import json
 import stripe
 
 from django.conf import settings
@@ -6,31 +5,27 @@ from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from elabodeal.api.serializers import NewPaymentIntentSerializer
+from elabodeal.api.serializers import NewPaymentIntentSerailizer
 
 
 class PaymentAPIView(APIView):
 	def post(self, request):
-		serializer = NewPaymentIntentSerializer(data=request.data)
+		serializer = NewPaymentIntentSerailizer(data=request.data)
 		if not serializer.is_valid():
 			errors = serializer.errors
 			return Response(errors, 400)
 
 		stripe.api_key = settings.STRIPE_API_KEY
-			
-		delivery = json.dumps(request.session['delivery'])
-		products = json.dumps(request.session['cart']['products'])
-
-		metadata={
-			'delivery': delivery,
-			'phone_number': serializer.data['phone_number'],
-			'products': products
-		}
-
+		
 		payment_intent = stripe.PaymentIntent.create(
 			amount=int(float(request.session['cart']['total_price']) * 100),
 			currency='PLN',
-			metadata=metadata
+			metadata={
+				'first_name': serializer.data['first_name'],
+				'last_name': serializer.data['last_name'],
+				'email': serializer.data['email'],
+				'phone_number': serializer.data['phone_number'],
+			}
 		)
 
 		return Response(payment_intent)
