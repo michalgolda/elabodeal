89eb4b67ed618f9d573641a0fcb96a9827934173
Maklero <michalgolda@vp.pl>
2020-07-23 14:48:38 +0200

    Refactor delete request session data

--- elabodeal/web/views/payment_success.py
+++ elabodeal/web/views/payment_success.py
@@ -4,12 +4,12 @@ from elabodeal.web.views.base import BaseView
 
 class PaymentSuccessView(BaseView):
 	def get(self, request):
-		if 'payment' not in request.session:
+		if 'delivery' not in request.session:
 			return redirect('web:index')
 
-		email = request.session['payment']['email']
+		email = request.session['delivery']['email']
 		
-		del request.session['payment']
+		del request.session['delivery']
 		del request.session['cart']
 
 		context = {
