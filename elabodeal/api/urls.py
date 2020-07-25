8924d0bb37c946aea231c99ea97a72f072e20e66
Maklero <michalgolda@vp.pl>
2020-07-25 14:13:01 +0200

    Add files api endpoint

--- elabodeal/api/urls.py
+++ elabodeal/api/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
+
 from elabodeal.api.views.webhook import WebHookAPIView
 from elabodeal.api.views.payment import PaymentAPIView
+from elabodeal.api.views.files import FilesAPIView
 
 app_name = 'api'
 
 urlpatterns = [
 	path('webhooks/', WebHookAPIView.as_view(), name='webhooks'),
-	path('payments/', PaymentAPIView.as_view(), name='payments')
+	path('payments/', PaymentAPIView.as_view(), name='payments'),
+	path('files/<str:name>/', FilesAPIView.as_view(), name='files')
 ]
