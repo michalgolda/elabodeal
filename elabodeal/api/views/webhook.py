4fa4ba108e1445cd1c8dd3da7f437f03fa98c59c
Maklero <michalgolda@vp.pl>
2020-07-26 14:40:33 +0200

    Refactor send email function parameters

--- elabodeal/api/views/webhook.py
+++ elabodeal/api/views/webhook.py
@@ -35,10 +35,13 @@ class WebHookAPIView(APIView):
 			delivery = json.loads(metadata['delivery'])
 			products = json.loads(metadata['products'])
 
+			product_objects = []
+
 			user = User.objects.filter(email=delivery['email']).first()
 			if user:
 				for p in products:
 					product = Product.objects.filter(id=p['id']).first()
+					product_objects.append(product)
 					has_purchased_product = PurchasedProduct.objects.filter(product__id=p['id']).first()
 					if not has_purchased_product:
 						purchased_product = PurchasedProduct(
@@ -54,14 +57,20 @@ class WebHookAPIView(APIView):
 					from_email=settings.EMAIL_HOST_USER,
 					recipient_list=[delivery['gift_email']],
 					html_message=render_to_string('emails/gift.html', {'email': delivery['gift_email']})
-				)				
+				)
+
+			context = {
+				'products': product_objects,
+				'total_price': '{0:.2f}'.format(round(event.data.object.amount / 100, 2)),
+				'payment_method': 'Karta płatnicza'
+			}
 
 			send_mail(
-				subject='Elabodeal - Nowa transakcja',
+				subject='Elabodeal - Nowa tranzakcja',
 				message=f'Dziękujemy za zakupy',
 				from_email=settings.EMAIL_HOST_USER,
 				recipient_list=[delivery['email']],
-				html_message=render_to_string('emails/new_transaction.html')
+				html_message=render_to_string('emails/payment_success.html', context)
 			)
 
 		return Response(status=200)
