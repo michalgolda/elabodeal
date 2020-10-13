b842889b3de64d4a908fc2c6ef5a5132dcf9da7b
Michał Gołda <michalgolda@vp.pl>
2020-10-13 18:24:59 +0200

    Add sentry

--- elabodeal/web/static/js/index.js
+++ elabodeal/web/static/js/index.js
@@ -1,11 +1,23 @@
-require( './nav.js' );
-require( './payment.js' );
-require( './scrollup.js' );
-require( './share-cart.js' );
-require( './update-product.js' );
-require( './delivery-form.js' );
-require( './product-detail.js' );
-require( './cart-add-popup.js' );
-require( './change-review-product-popup.js' );
-require( './review-product-popup.js' );
-require( './add-product.js' );
+import { env } from 'process';
+
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
+
+Sentry.init(	{
+  dsn: "https://3785464099b948e18884fe59dfbc438e@o320975.ingest.sentry.io/5367878",
+  integrations: [ new Integrations.BrowserTracing() ],
+  debug: env.NODE_ENV === 'production' ? false : true
+}	);
+
+import './nav.js';
+import './payment.js';
+import './scrollup.js';
+import './share-cart.js';
+import './update-product.js';
+import './delivery-form.js';
+import './product-detail.js';
+import './cart-add-popup.js';
+import './change-review-product-popup.js';
+import './review-product-popup.js';
+import './add-product.js';
