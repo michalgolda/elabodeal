31b0c467fee27e6fb5988f397b372f350744614d
Michał Gołda <michagolda@vp.pl>
2021-01-04 22:54:37 +0100

    Refactor navigation scripts

--- elabodeal/web/static/js/nav.js
+++ elabodeal/web/static/js/nav.js
@@ -1,18 +1,34 @@
-$( '#nav-menu-open' ).click( function() {
-    if( $( '#nav-menu' ).is( ':hidden' ) ) {
-        
-        $( '#nav-menu' ).show();
-        $( '#nav-arrow-down' ).hide();
-        $( '#nav-arrow-up' ).show(); 
-    } else {
-        $( '#nav-arrow-down' ).show();
-        $( '#nav-arrow-up' ).hide();
-        $( '#nav-menu' ).hide();
-    }
-})
+( function( document ){
+    document.addEventListener( 'DOMContentLoaded', function(){
+        var navShowMenu = document.querySelector( '#js-show-menu' );
+        var navMenu = document.querySelector( '#js-menu' );
 
-$( '#nav-menu' ).hover( function(){}, function() {
-    $( this ).hide();
-    $( '#nav-arrow-down' ).show();
-    $( '#nav-arrow-up' ).hide();
-})
+        if ( !navShowMenu || !navMenu ) return;
+
+        // Get arrow icons
+        arrowDownIcon = $( navShowMenu.children[ 2 ] );
+        arrowUpIcon = $( navShowMenu.children[ 3 ] );
+
+        /* Show/Hide popup with menu on click for btn in top navigation */
+        navShowMenu.addEventListener( 'click', function(){
+            if ( $( navMenu ).is( ':hidden' ) ) {
+                $( navMenu ).show();
+                
+                arrowDownIcon.hide();
+                arrowUpIcon.show();
+            } else {
+                $( navMenu ).hide();
+
+                arrowUpIcon.hide();
+                arrowDownIcon.show();
+            }
+        } );
+
+        /* Hide popup on mouse out with menu area */
+        $( navMenu ).hover( function(){}, function(){
+            $( this ).hide();
+            $( arrowDownIcon ).show();
+            $( arrowUpIcon ).hide();
+        } );
+    } );
+} )( document );    
