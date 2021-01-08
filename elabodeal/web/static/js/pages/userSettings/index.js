00e9dddbd966fb541f6caec5c84f3a111b250326
Michał Gołda <michagolda@vp.pl>
2021-01-08 19:00:21 +0100

    Add modules

--- elabodeal/web/static/js/pages/userSettings/index.js
+++ elabodeal/web/static/js/pages/userSettings/index.js
@@ -2,3 +2,7 @@ import './publisherSettingsSection';
 import './saveSettingsForm';
 import './editOptionButtons';
 import './cancelEditOptionButtons';
+import './startEmailVerificationBtn';
+import './closeEmailVerificationModalBtn';
+import './verificationEmailForm';
+import './resendEmailVerificationCodeBtn';
