5a2875fe9304e44aaa7d0b76cf08e65c4fa92be3
Michał Gołda <michagolda@vp.pl>
2021-01-08 17:48:01 +0100

    Remove APITestCase class

--- tests/base.py
+++ tests/base.py
@@ -2,16 +2,10 @@ import unittest
 
 from django.test import TestCase
 
-from rest_framework.test import APITestCase as BaseAPITestCase
-
 
 class BaseTestCase(unittest.TestCase):
 	pass
 
 
 class WebTestCase(BaseTestCase, TestCase):
-	pass
-
-
-class APITestCase(BaseTestCase, BaseAPITestCase):
 	pass
