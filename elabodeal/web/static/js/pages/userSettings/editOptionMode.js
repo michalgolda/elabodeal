3866813f4def2d28120103a91ccae2aaa640c525
Michał Gołda <michagolda@vp.pl>
2021-01-12 18:54:33 +0100

    Add disable and toggle edit option mode utils

---
+++ elabodeal/web/static/js/pages/userSettings/editOptionMode.js
@@ -0,0 +1,74 @@
+
+/** 
+ * @param { HTMLElement } trigger - The element are in div which has a option__body class
+ */
+function ToggleEditOptionMode ( trigger ){
+    var currentEditingOption = window.currentEditingOption;
+
+    // Disable edit mode of last edited option
+    if ( currentEditingOption ) 
+        DisableEditOptionMode( currentEditingOption );
+    
+    var optionContainer = trigger.parentNode;
+    
+    currentEditingOption = trigger;
+    
+    window.currentEditingOption = currentEditingOption;
+
+    var $optionContainer = $( optionContainer );
+    var $optionInput = $optionContainer.find( '#js-option-input' );
+    var $optionToggleModeBtn = $optionContainer.find( '#js-edit-option-btn' );
+    var $optionCancelEditBtn = $optionContainer.find( '#js-cancel-edit-option-btn' );
+    var $optionAcceptEditBtn = $optionContainer.find( '#js-accept-edit-option-btn' );
+
+    $optionToggleModeBtn.hide();
+
+    $optionAcceptEditBtn.show();
+    $optionCancelEditBtn.show();
+
+    $optionContainer.addClass( 'option__body-editing' );
+
+    $optionInput.focus();
+    $optionInput.select();
+    $optionInput.attr( 'readOnly', false );
+}
+
+/** 
+ * @param { HTMLElement } trigger - The element are in div which has a option__body class
+ * @param { Boolean } keepInputValue - Save or not input value
+ */
+function DisableEditOptionMode ( trigger, keepInputValue = false ){
+    var currentEditingOption = window.currentEditingOption;
+
+    // If has exist disable edit mode of last edited option else ingore
+    if ( currentEditingOption && trigger === undefined )
+        trigger = currentEditingOption;
+    else if ( !trigger ) return;
+    
+    delete window.currentEditingOption;
+
+    var optionContainer = trigger.parentNode;
+
+    var $optionContainer = $( optionContainer );
+    var $optionInput = $optionContainer.find( '#js-option-input' );
+    var $optionToggleModeBtn = $optionContainer.find( '#js-edit-option-btn' );
+    var $optionCancelEditBtn = $optionContainer.find( '#js-cancel-edit-option-btn' );
+    var $optionAcceptEditBtn = $optionContainer.find( '#js-accept-edit-option-btn' );
+
+    $optionAcceptEditBtn.hide();
+    $optionCancelEditBtn.hide();
+
+    $optionToggleModeBtn.show();
+
+    $optionContainer.removeClass( 'option__body-editing' );
+
+    $optionInput.attr( 'readOnly', true );
+
+    if ( !keepInputValue )
+        $optionInput.val( $optionInput.attr( 'value' ) );
+
+    // Clear selection of text
+    window.getSelection().removeAllRanges();
+}
+
+export { DisableEditOptionMode, ToggleEditOptionMode };
