d48a5ab4f60fee2776c3df75ce0f648c44407494
Michał Gołda <michagolda@vp.pl>
2021-01-12 18:53:25 +0100

    Refactor submit listener and add skip submit by Enter key

--- elabodeal/web/static/js/pages/userSettings/saveSettingsForm.js
+++ elabodeal/web/static/js/pages/userSettings/saveSettingsForm.js
@@ -1,12 +1,42 @@
-import { SaveSettingsListener } from './saveSettingsListener';
+import alert from '../../alert';
+import { DisableEditOptionMode } from './editOptionMode';
 
 
-( function( document ){
-    document.addEventListener( 'DOMContentLoaded', function(){
-        var form = document.querySelector( '#js-settings-form' );
+( function( document, $ ){
+    document.addEventListener( 'DOMContentLoaded', () => {
+        var form = document.getElementById( 'js-settings-form' );
 
         if ( !form ) return;
 
-        form.addEventListener( 'submit', SaveSettingsListener );
+        var $form = $( form );
+    
+        form.addEventListener( 'keypress', ( e ) => {
+            if ( e.keyCode === 13 ) {
+                e.preventDefault();
+
+                DisableEditOptionMode( undefined, true );
+            }
+        } );
+
+        form.addEventListener( 'submit', ( e ) => {
+            e.preventDefault();
+
+            DisableEditOptionMode();
+
+            var actionURL = $form.attr( 'action' );
+            var csrfToken = $( 'meta[name="csrf_token"]' ).attr( 'content' );
+            var formData = $form.serializeArray();
+
+            $.ajax( {
+                url: actionURL,
+                method: 'POST',
+                headers: {
+                    'X-CSRFToken': csrfToken
+                },
+                data: formData,
+                success: () => alert( 'Ustawienia zostały pomyślnie zapisane', 'success' ),
+                error: () => alert( 'Coś poszło nie tak. Spróbuj ponownie.', 'error' )
+            } )
+        } );
     } );
-} )( document );
+} )( document, $ );
