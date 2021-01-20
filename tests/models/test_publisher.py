22f1181e398605b0c141028b9c9c5110a1ce87d4
Michał Gołda <michagolda@vp.pl>
2021-01-20 17:37:14 +0100

    Add test update_settings method

--- tests/models/test_publisher.py
+++ tests/models/test_publisher.py
@@ -4,6 +4,15 @@ from elabodeal.models import Publisher
 
 
 class TestPublisherModel(BaseTestCase):
+    def setUp(self):
+        self.publisher = Publisher.objects.create_publisher(
+            first_name='test',
+            last_name='test',
+            account_number='123',
+            country='PL',
+            swift='123'
+        ) 
+    
     def test_fields(self):
         self.assertEqual(hasattr(Publisher, 'first_name'), True)
         self.assertEqual(hasattr(Publisher, 'last_name'), True)
@@ -11,3 +20,16 @@ class TestPublisherModel(BaseTestCase):
         self.assertEqual(hasattr(Publisher, 'country'), True)
         self.assertEqual(hasattr(Publisher, 'swift'), True)
         self.assertEqual(hasattr(Publisher, 'sell_notification'), True)
+
+    def test_manager_create_publisher_method(self):
+        self.assertIsInstance(self.publisher, Publisher)
+
+    def test_manager_update_settings_method(self):
+        options = {
+            'first_name': 'test123'
+        }
+
+        updated_publisher = Publisher.objects.update_settings(self.publisher, options)
+
+        self.assertIsInstance(self.publisher, Publisher)
+        self.assertEqual(updated_publisher.first_name, 'test123')
