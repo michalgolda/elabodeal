b124a81f606f314be11e1a485579d1e47d76cd8f
Michał Gołda <michagolda@vp.pl>
2021-01-20 19:23:21 +0100

    Add test for publisher settings view

---
+++ tests/web/views/test_publisher_settings.py
@@ -0,0 +1,37 @@
+from tests import WebTestCase
+from django.urls import reverse
+
+from elabodeal.models import User
+
+
+class TestPublisherSettingsView(WebTestCase):
+    def setUp(self):
+        self.url = url = reverse('web:publisher-settings')
+
+    def test_simple_response(self):
+        user = User.objects.create_user(
+            email='xyz@xyz.pl',
+            username='xyz',
+            password='xyz'
+        )
+
+        self.client.login(
+            email=user.email, 
+            password='xyz'
+        )
+
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'publisher-settings.html')
+
+    def test_response_if_user_is_not_logged(self):
+        response = self.client.get(self.url)
+
+        self.assertRedirects(
+            response, 
+            reverse('web:login'), 
+            status_code=302, 
+            target_status_code=200, 
+            fetch_redirect_response=True
+        )
