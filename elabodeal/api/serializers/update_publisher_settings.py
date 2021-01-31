ce549a99e4299757f156f5ca1e67b6f4d7186feb
Michał Gołda <michagolda@vp.pl>
2021-01-31 17:09:28 +0100

    refactor: change serializer class name

--- elabodeal/api/serializers/update_publisher_settings.py
+++ elabodeal/api/serializers/update_publisher_settings.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 
-class UpdatePublisherSettingsSerializer(serializers.Serializer):
+class UpdatePublisherSettingsRequestSerializer(serializers.Serializer):
     first_name = serializers.CharField(max_length=30)
     last_name = serializers.CharField(max_length=30)
     account_number = serializers.CharField()
