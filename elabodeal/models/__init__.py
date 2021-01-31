320850705f1d23f0e0059ec86b40d8b754c79ed8
Michał Gołda <michagolda@vp.pl>
2021-01-31 17:08:26 +0100

    refactor: change verification code imports

--- elabodeal/models/__init__.py
+++ elabodeal/models/__init__.py
@@ -9,8 +9,5 @@ from .shared_cart import SharedCart
 from .cart import Cart
 from .cart_item import CartItem
 from .purchased_product import PurchasedProduct
-from .verification_code import (
-    VerificationCode, 
-    VerificationCodeException
-)
+from .verification_code import VerificationCode
 from .banned_user import BannedUser
