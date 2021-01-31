47ea4d4b9c848caa07a6a96472c974630df20aca
Michał Gołda <michagolda@vp.pl>
2021-01-31 17:13:12 +0100

    refactor: add interactor execute and change endpoint class name

--- elabodeal/api/endpoints/update_user_settings.py
+++ elabodeal/api/endpoints/update_user_settings.py
@@ -1,20 +1,20 @@
 from elabodeal.api.endpoints import Endpoint
-from elabodeal.api.serializers import UpdateUserSettingsSerializer
-
-from elabodeal.models import User
+from elabodeal.api.serializers import UpdateUserSettingsRequestSerializer
+from elabodeal.api.interactors import UpdateUserSettingsInteractor
 
 
 class UpdateUserSettingsEndpoint(Endpoint):
     def put(self, request):
-        serializer = UpdateUserSettingsSerializer(
+        serializer = UpdateUserSettingsRequestSerializer(
             data=request.data
         )
         serializer.is_valid(raise_exception=True)
 
-        user = request.user
-        options = serializer.data
-
-        User.objects.update_settings(user, options)
+        with UpdateUserSettingsInteractor() as interactor:
+            interactor.execute(
+                request.user,
+                serializer.data
+            )
 
         return self.respond(status=200)
 
