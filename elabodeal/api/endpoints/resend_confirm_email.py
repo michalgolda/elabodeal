30acf06d4ff1efefd3605a865910696105bd3a2e
Michał Gołda <michagolda@vp.pl>
2021-01-31 21:22:30 +0100

    feat: add user, code repository

--- elabodeal/api/endpoints/resend_confirm_email.py
+++ elabodeal/api/endpoints/resend_confirm_email.py
@@ -1,6 +1,7 @@
 from elabodeal.api.endpoints import Endpoint
 from elabodeal.api.serializers import ResendConfirmEmailRequestSerializer
 from elabodeal.api.interactors import ResendConfirmEmailInteractor
+from elabodeal.api.repositories import UserRepository, VerificationCodeRepository
 
 
 class ResendConfirmEmailEndpoint(Endpoint):
@@ -10,7 +11,13 @@ class ResendConfirmEmailEndpoint(Endpoint):
         )
         serializer.is_valid(raise_exception=True)
 
-        with ResendConfirmEmailInteractor() as interactor:
+        code_repository = VerificationCodeRepository()
+        user_repository = UserRepository()
+
+        with ResendConfirmEmailInteractor(
+            user_repo=user_repository,
+            code_repo=code_repository
+        ) as interactor:
             interactor.execute(**serializer.data)
 
         return self.respond(status=200)
