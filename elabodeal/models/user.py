2a5c4b747d3ca59a226d232609c2cb0c3d723af2
Michał Gołda <michagolda@vp.pl>
2021-01-31 17:07:15 +0100

    refactor: remove update_settings method and add type hints

--- elabodeal/models/user.py
+++ elabodeal/models/user.py
@@ -1,19 +1,23 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import (AbstractBaseUser, 
-										BaseUserManager)
+from django.contrib.auth.models import (
+	AbstractBaseUser, 
+	BaseUserManager
+)
 
 
 class UserManager(BaseUserManager):
-	def create_user(self, email, username, password):
-		user = self.model(email=BaseUserManager.normalize_email(email),
-						  username=username)
+	def create_user(self, email: str, username: str, password: str) -> object:
+		user = self.model(
+			email=BaseUserManager.normalize_email(email),
+			username=username
+		)
 		user.set_password(password)
 		user.save()
 
 		return user
 
-	def create_superuser(self, email, username, password):
+	def create_superuser(self, email: str, username: str, password: str) -> object:
 		user = self.create_user(email, username, password)
 		user.is_staff = True
 		user.is_superuser = True
@@ -24,31 +28,13 @@ class UserManager(BaseUserManager):
 
 		return user
 
-	def update_settings(self, user, options):
-		has_changed = False
-
-		for attr_name, attr_value in options.items():
-			if getattr(user, attr_name) != attr_value and attr_name != None:
-				setattr(user, attr_name, attr_value)
-
-				# Force verification for email address
-				if attr_name == 'email':
-					user.email_verified = False
-
-				has_changed = True
-
-		if not has_changed:
-			return
-
-		user.save()
-
-		return user
-
 
 class User(AbstractBaseUser):
-	publisher = models.OneToOneField('elabodeal.Publisher',
-							  		 on_delete=models.CASCADE,
-							  		 null=True)
+	publisher = models.OneToOneField(
+		'elabodeal.Publisher',
+		on_delete=models.CASCADE,
+		null=True
+	)
 
 	username = models.CharField(max_length=15)
 	email = models.EmailField(unique=True)
@@ -70,7 +56,6 @@ class User(AbstractBaseUser):
 	REQUIRED_FIELDS = ['username']
 
 	objects = UserManager()
-
 
 	def has_perm(self, perm, obj=None):
 		return self.is_superuser
