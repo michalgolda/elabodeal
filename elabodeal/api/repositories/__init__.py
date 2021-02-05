2947b7934233d60d5f6a9d03a48fbb9654cc2518
Michał Gołda <michagolda@vp.pl>
2021-02-05 23:21:35 +0100

    feat: add CartRepository, ProductRepository import

--- elabodeal/api/repositories/__init__.py
+++ elabodeal/api/repositories/__init__.py
@@ -2,3 +2,5 @@ from .base import Repository
 from .user import UserRepository
 from .verification_code import VerificationCodeRepository
 from .publisher import PublisherRepository
+from .cart import CartRepository
+from .product import ProductRepository
