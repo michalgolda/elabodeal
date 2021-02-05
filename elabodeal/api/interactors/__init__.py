8f4dff2babeffcbe07ad8ff457db01b0d36eecdd
Michał Gołda <michagolda@vp.pl>
2021-02-05 23:20:44 +0100

    feat: add SaveCartInteractor import

--- elabodeal/api/interactors/__init__.py
+++ elabodeal/api/interactors/__init__.py
@@ -3,3 +3,4 @@ from .confirm_email import ConfirmEmailInteractor
 from .resend_confirm_email import ResendConfirmEmailInteractor
 from .update_publisher_settings import UpdatePublisherSettingsInteractor
 from .update_user_settings import UpdateUserSettingsInteractor
+from .save_cart import SaveCartInteractor
