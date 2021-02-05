d52568bd1c13919f7c2975505e32849752104e72
Michał Gołda <michagolda@vp.pl>
2021-02-05 20:48:06 +0100

    feat: add default alert message constants

---
+++ elabodeal/web/static/js/constants.js
@@ -0,0 +1,14 @@
+export const ALERT_MESSAGES = {
+    CONFIRM_EMAIL_ERROR: 'Podany kod jest błędny. Spróbuj ponownie.',
+    CONFIRM_EMAIL_SUCCESS: 'Twój adres email został poprawnie zweryfikowany.',
+    RESEND_CONFIRM_EMAIL_SUCCESS: 'Nowy kod został wysłany na adres email.',
+
+    SETTINGS_SAVE_SUCCESS: 'Ustawienia zostały pomyślnie zapisane.',
+
+    CART_SAVE_SUCCESS: 'Koszyk został pomyślnie zapisany.',
+    
+    BAD_REQUEST: 'Podane dane są nieprawidłowe. Spróbuj ponownie.',
+    SERVER_ERROR: `Wystąpił błąd po stronie serwera. 
+                   Po zlokalizowaniu usterki, 
+                   postaramy się jak najszybciej ją usunąć :)`
+}
