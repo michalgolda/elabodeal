ebbbaa5a3811e21ad30a195c40c99d55777ee5b0
Michał Gołda <michagolda@vp.pl>
2021-02-05 23:20:27 +0100

    feat: add SaveCartEndpoint import

--- elabodeal/api/endpoints/__init__.py
+++ elabodeal/api/endpoints/__init__.py
@@ -3,3 +3,4 @@ from .confirm_email import ConfirmEmailEndpoint
 from .resend_confirm_email import ResendConfirmEmailEndpoint
 from .update_publisher_settings import UpdatePublisherSettingsEndpoint
 from .update_user_settings import UpdateUserSettingsEndpoint
+from .save_cart import SaveCartEndpoint
