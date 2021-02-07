839bde5ab81343d3fe28f268f08acfd4ac457f44
Michał Gołda <michagolda@vp.pl>
2021-02-07 20:49:05 +0100

    refactor: delete useless code

--- elabodeal/models/shared_cart.py
+++ elabodeal/models/shared_cart.py
@@ -6,22 +6,22 @@ from django.shortcuts import reverse
 
 
 class SharedCartManager(models.Manager):
-	def create_share(self, cart):
-		shared_cart = self.model.objects.filter(cart=cart).first()
-
-		if not shared_cart:			
-				code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
-
-				shared_cart = self.model.objects.create(code=code, 
-														cart=cart)
+	def create_share(self, cart: object) -> object:
+		code = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+		shared_cart = self.model(
+			code=code,
+			cart=cart
+		)
 
 		return shared_cart
 
 
 class SharedCart(models.Model):
 	code = models.CharField(max_length=6)
-	cart = models.ForeignKey('elabodeal.Cart', 
-					  		 on_delete=models.CASCADE)
+	cart = models.ForeignKey(
+		'elabodeal.Cart', 
+		on_delete=models.CASCADE
+	)
 
 	shared_at = models.DateTimeField(auto_now_add=True)
 
