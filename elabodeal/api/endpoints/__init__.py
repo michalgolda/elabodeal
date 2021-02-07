9c4f9cfbe476cbf706898331cb8db61029a79552
Michał Gołda <michagolda@vp.pl>
2021-02-07 20:47:07 +0100

    feat: add ShareCartEndpoint import

--- elabodeal/api/endpoints/__init__.py
+++ elabodeal/api/endpoints/__init__.py
@@ -4,3 +4,4 @@ from .resend_confirm_email import ResendConfirmEmailEndpoint
 from .update_publisher_settings import UpdatePublisherSettingsEndpoint
 from .update_user_settings import UpdateUserSettingsEndpoint
 from .save_cart import SaveCartEndpoint
+from .share_cart import ShareCartEndpoint
