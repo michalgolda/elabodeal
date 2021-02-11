7f64a241e6adada508dad38db04628e5dfe07a91
Michał Gołda <michagolda@vp.pl>
2021-02-11 17:02:50 +0100

    refactor: add default webpack config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,80 +1,20 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
+const webpackConfig = require( './webpack.config' );
 
 // Plugins
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-	
 
-const paths = {
-	'dist': path.resolve(
-		__dirname, 
-		'elabodeal/web/static/dist/'
-	),
-	'mainEntrances': [
-		path.resolve(
-			__dirname, 
-			'elabodeal/web/static/js/main.js'
-		),
-		path.resolve(
-			__dirname,
-			'elabodeal/web/static/styles/main.scss'
-		),
-	]
-}
-
-module.exports = {
-	mode: 'production',
-	devtool: 'source-map',
-	entry: {
-		app: paths.mainEntrances
-	},
-	output: {
-		filename: 'main.js',
-		path: paths.dist
-	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: 'main.css',
+webpackConfig.mode = 'production';
+webpackConfig.devtool = 'source-map';
+webpackConfig.optimization = {
+	...webpackConfig.optimization,
+	minimize: true,
+	minimizer: [
+		new CssMinimizerPlugin(),
+		new TerserPlugin({
+			sourceMap: true,
 		}),
-	],
-	module: {
-		rules: [
-			{
-				test: /\.(css|sass|scss)$/,
-        		use: [
-        			MiniCssExtractPlugin.loader, 
-        			{
-        				loader: 'css-loader',
-        				options: {
-        					sourceMap: true
-        				}
-        			},
-        			{
-        				loader: 'sass-loader',
-        				options: {
-        					sourceMap: true
-        				},
-        			},
-        		], 
-			},
-			{
-				test: /\.m?js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader"
-				}
-			}
-		],
-	},
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new CssMinimizerPlugin(),
-			new TerserPlugin({
-				sourceMap: true,
-			}),
-		],
-	},
+	]
 };
+
+module.exports = webpackConfig;
