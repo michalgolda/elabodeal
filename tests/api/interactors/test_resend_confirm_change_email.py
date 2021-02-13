7c93c4b7f52bb7727f729953ef715792e4f6c47f
Michał Gołda <michagolda@vp.pl>
2021-02-13 14:49:28 +0100

    test: add test for resend confirm change email interactor

---
+++ tests/api/interactors/test_resend_confirm_change_email.py
@@ -0,0 +1,59 @@
+from unittest import mock
+from tests import BaseTestCase
+
+from django.test import override_settings
+from django.core import mail
+
+from elabodeal.api.interactors import ResendConfirmChangeEmailInteractor
+from elabodeal.api.exceptions import ErrorRegistry
+
+
+class ResendConfirmChangeEmailInteractorTest(BaseTestCase):
+    
+    @mock.patch('elabodeal.api.repositories.UserRepository')
+    @mock.patch('elabodeal.api.repositories.VerificationCodeRepository')
+    def setUp(self, mock_user_repo, mock_code_repo):
+        self.mock_user_repo = mock_user_repo
+        self.mock_code_repo = mock_code_repo
+
+        mail.outbox = []
+
+    @override_settings(
+        EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend',
+        task_always_eager=True
+    )
+    def test_execute(self):
+        self.mock_user_repo.find_by_email.return_value = True
+        self.mock_code_repo.find_by_email.return_value = None
+        
+        with ResendConfirmChangeEmailInteractor(
+                user_repo=self.mock_user_repo,
+                code_repo=self.mock_code_repo
+            ) as interactor:
+                interactor.execute(email='test@test.pl')
+
+        self.assertEqual( self.mock_code_repo.save.called, True )
+        self.assertEqual(len(mail.outbox), 1)
+
+    def test_execute_if_user_does_not_exist(self):
+        self.mock_user_repo.find_by_email.return_value = None
+
+        with self.assertRaises(ErrorRegistry.RESEND_CONFIRM_EMAIL):
+            with ResendConfirmChangeEmailInteractor(
+                user_repo=self.mock_user_repo,
+                code_repo=self.mock_code_repo
+            ) as interactor:
+                interactor.execute(email='test')
+
+    @override_settings(task_always_eager=True)
+    def test_execute_remove_existing_code(self):
+        self.mock_user_repo.find_by_email.return_value = True
+        self.mock_code_repo.find_by_email.return_value = True
+
+        with ResendConfirmChangeEmailInteractor(
+            user_repo=self.mock_user_repo,
+            code_repo=self.mock_code_repo
+        ) as interactor:
+            interactor.execute(email='test')
+
+        self.assertEqual(self.mock_code_repo.delete.called, True)
