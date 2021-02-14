fcc92e68ca9398c2173fa222595b9e45690cc3b9
Michal001 <michalgolda@vp.pl>
2021-02-14 13:12:52 +0100

    feat: add email confirmation required information for user settings page

---
+++ elabodeal/web/static/js/pages/settings/userSettings/components/EmailConfirmationRequiredInfo.vue
@@ -0,0 +1,13 @@
+<template>
+	<div style="margin-top: 1rem;">
+	    <p class="option__information" style="margin-bottom: .5rem;">
+	    	Aby móc w pełni korzystać z funkcjonalnośći platformy musisz zweryfikować swój adres email.
+	    </p>
+	    <button type="button" class="btn btn__secondary">Zweryfikuj</button>
+	</div>
+</template>
+<script>
+export default {
+
+}
+</script>
