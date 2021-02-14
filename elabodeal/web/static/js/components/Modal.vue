f074ac78f5229ca9c153ffa649cfaa8853ea5b76
Michal001 <michalgolda@vp.pl>
2021-02-14 19:30:18 +0100

    feat: add Modal component

---
+++ elabodeal/web/static/js/components/Modal.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="modal-wrapper flex-center">
+		<div class="modal">
+			<div class="modal__header">
+				<p class="modal__title">{{ title }}</p>
+				<i 
+					class="fas fa-times modal__close-btn"
+					@click="$modal.hide()"
+				></i>
+			</div>
+			<div class="modal__body">
+				<slot />
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		title: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
