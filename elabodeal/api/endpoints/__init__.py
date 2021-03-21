71e9961570a45bc53759bf09f8d9cc44be7f12e8
Michal001 <michalgolda@vp.pl>
2021-03-21 17:21:07 +0100

    feat: add imports of product group endpoints

--- elabodeal/api/endpoints/__init__.py
+++ elabodeal/api/endpoints/__init__.py
@@ -1,8 +1,12 @@
 from .base import Endpoint
-from .confirm_email import ConfirmEmailEndpoint
-from .resend_confirm_email import ResendConfirmEmailEndpoint
-from .update_publisher_settings import UpdatePublisherSettingsEndpoint
-from .update_user_settings import UpdateUserSettingsEndpoint
-from .save_cart import SaveCartEndpoint
-from .share_cart import ShareCartEndpoint
-from .resend_confirm_change_email import ResendConfirmChangeEmailEndpoint
+# from .confirm_email import ConfirmEmailEndpoint
+# from .resend_confirm_email import ResendConfirmEmailEndpoint
+# from .update_publisher_settings import UpdatePublisherSettingsEndpoint
+# from .update_user_settings import UpdateUserSettingsEndpoint
+# from .save_cart import SaveCartEndpoint
+# from .share_cart import ShareCartEndpoint
+# from .resend_confirm_change_email import ResendConfirmChangeEmailEndpoint
+from .product_group import (
+	MeProductsGroupsEndpoint,
+	MeProductsGroupsDetailsEndpoint
+)
