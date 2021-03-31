3af83f5395cfa6b010038888ae345534f89624d2
Michal001 <michalgolda@vp.pl>
2021-03-31 18:06:07 +0200

    refactor: add functions which are required

--- elabodeal/api/repositories/product.py
+++ elabodeal/api/repositories/product.py
@@ -3,13 +3,24 @@ from elabodeal.models import Product
 
 
 class ProductRepository(Repository):
-    def get(self, id: int) -> Product:
-        return Product.objects.filter(id=id).first()
+	
+	def add(self, files, other_images, *args, **kwargs):
+		product = Product(*args, **kwargs)
+		product.save()
+		
+		for file in files:
+			product.files.add(file)
 
-    def delete(self, product: Product) -> None:
-        product.delete()
+		for other_image in other_images:
+		   product.other_images.add(other_image)
+		
+		return product
 
-    def save(self, product: Product) -> Product:
-        product.save()
+	def get_all_by(self, *args, **kwargs):
+		return Product.objects.filter(*args, **kwargs).all()
 
-        return product
+	def get_one_by(self, *args, **kwargs):
+		return Product.objects.filter(*args, **kwargs).first()
+
+	def delete_by(self, *args, **kwargs):
+		return any(Product.objects.filter(*args, **kwargs).delete())
