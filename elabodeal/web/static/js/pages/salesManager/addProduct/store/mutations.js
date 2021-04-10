cb02c73c1313f8555d66396bb7c0d5423db1418c
Michal001 <michalgolda@vp.pl>
2021-04-10 22:12:15 +0200

    feat: add 'clearFieldError' mutation and refactor 'validateFormData' mutation

--- elabodeal/web/static/js/pages/salesManager/addProduct/store/mutations.js
+++ elabodeal/web/static/js/pages/salesManager/addProduct/store/mutations.js
@@ -1,8 +1,42 @@
 const mutations = {
 	updateFormData: function ( state, payload ) {
 		const { fieldName, fieldValue } = payload;
+		const field = state.form.fields[ fieldName ];
 
-		state.formData[ fieldName ] = fieldValue;
+		if ( field.error ) field.error = false;
+
+		field.value = fieldValue;
+	},
+	validateFormData: function ( state ) {
+		const fieldNames = Object.keys( state.form.fields );
+
+		var isValid = true;
+
+		for ( var fieldName of fieldNames ) {
+			const field = state.form.fields[ fieldName ];
+
+			if ( field.required ) {
+				if ( field.value instanceof Array ) {
+					if ( field.value.length === 0 ) {
+						field.error = true;
+						isValid = false;
+					}
+				} else {
+					if ( field.value === null ) {
+						field.error = true;
+						isValid = true;
+					}
+				}
+			}
+		}
+
+		state.form.isValid = isValid;
+	},
+	clearFieldError: function ( state, payload ) {
+		const { fieldName } = payload;
+		const field = state.form.fields[ fieldName ];
+
+		field.error = false;
 	}
 }
 
