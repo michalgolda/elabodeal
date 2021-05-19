ea3a341c32ca04cf1fbb1c2128bf153a1a2c8677
Michal001 <michalgolda@vp.pl>
2021-05-19 21:25:25 +0200

    refactor: create separated PageCountInput and PublishedYearInput component
    This components are based on NumberInputControls component

---
+++ elabodeal/web/static/js/pages/salesManager/addProduct/components/PageCountInput.vue
@@ -0,0 +1,81 @@
+<template>
+	<number-input-controls
+		@incrementValue="handleIncrementValue"
+		@decrementValue="handleDecrementValue"
+	>
+		<input 
+			type="number"
+			name="page_count"
+			:class="{ 'form__input-error': page_count.error }"
+			:value="page_count.value"
+			@change="handleChangeValue"
+			@keyup="$store.commit( 'clearFieldError', { fieldName: 'page_count' } )"
+		/>
+	</number-input-controls>
+</template>
+<script>
+import { mapState } from "vuex";
+
+import NumberInputControls from "../../../../components/NumberInputControls.vue";
+
+export default {
+	computed: mapState( {
+		page_count: state => state.form.fields.page_count
+	} ),
+	components: {
+		NumberInputControls
+	},
+	methods: {
+		handleIncrementValue: function ( e ) {
+			const value = this.page_count.value ? this.page_count.value : 0;
+
+			const updatedValue = value + 1;
+
+			this.$store.commit(
+				"updateFormData",
+				{
+					fieldName: "page_count",
+					fieldValue: updatedValue
+				}
+			);
+		},
+		handleDecrementValue: function ( e ) {
+			const value = this.page_count.value ? this.page_count.value : 0;
+
+			const updatedValue = value - 1;
+
+			if ( updatedValue <= 0 ) {
+				this.$store.commit(
+					"updateFormData",
+					{
+						fieldName: "page_count",
+						fieldValue: null
+					}
+				);
+
+				return;
+			}
+
+			this.$store.commit(
+				"updateFormData",
+				{
+					fieldName: "page_count",
+					fieldValue: updatedValue
+				}
+			);
+		},
+		handleChangeValue: function ( e ) {
+			const input = e.target;
+			const inputValue = Number.parseInt( input.value );
+
+			this.$store.commit(
+				"updateFormData",
+				{
+					fieldName: "page_count",
+					fieldValue: inputValue
+				}
+			);
+		}
+	}
+}
+</script>
