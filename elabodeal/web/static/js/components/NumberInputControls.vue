d1dd7ae598d0977cb8114048fb577aa66e7e2c2d
Michal001 <michalgolda@vp.pl>
2021-05-19 21:21:56 +0200

    feat: add NumberInputControls component

---
+++ elabodeal/web/static/js/components/NumberInputControls.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="number-input-controls">
+		<button 
+			class="btn btn__secondary" 
+			type="button"
+			@click="$emit( 'decrementValue' )"
+		>
+			<i class="fas fa-minus"></i>
+		</button>
+		<div class="input-wrapper">
+			<slot />
+		</div>
+		<button 
+			class="btn btn__secondary" 
+			type="button"
+			@click="$emit( 'incrementValue' )"
+		>
+			<i class="fas fa-plus"></i>
+		</button>
+	</div>
+</template>
+<script>
+export default {
+	emits: [ "incrementValue", "decrementValue" ]
+}
+</script>
