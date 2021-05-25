9b3b6c912bf6a5a7dea5829c663c58808bfc6328
Michal001 <michalgolda@vp.pl>
2021-05-25 22:09:15 +0200

    chore: change devtool to "eval"

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,7 @@
 const webpackConfig = require( './webpack.config' );
 
 webpackConfig.mode = 'development';
-webpackConfig.devtool = 'cheap-module-eval-source-map';
+webpackConfig.devtool = 'eval';
 webpackConfig.watch = true;
 webpackConfig.resolve = {
     alias: {
