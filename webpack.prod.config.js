1723e83466706384b19f47cadf52683ad121060c
Michal001 <michalgolda@vp.pl>
2021-05-26 09:09:16 +0200

    chore: change devtool to eval

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -5,15 +5,12 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
 
 webpackConfig.mode = 'production';
-webpackConfig.devtool = 'source-map';
 webpackConfig.optimization = {
 	...webpackConfig.optimization,
 	minimize: true,
 	minimizer: [
 		new CssMinimizerPlugin(),
-		new TerserPlugin({
-			sourceMap: true,
-		}),
+		new TerserPlugin(),
 	]
 };
 webpackConfig.resolve = {
