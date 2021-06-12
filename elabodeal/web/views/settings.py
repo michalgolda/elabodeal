cebc4adaa3b02843ca3f018a7cbd9a2a8a7172ed
Michal001 <michalgolda@vp.pl>
2021-06-12 23:14:52 +0200

    feat: add settings view and template

---
+++ elabodeal/web/views/settings.py
@@ -0,0 +1,9 @@
+import json
+from elabodeal.web.views import BaseView
+
+
+class SettingsView(BaseView):
+	auth_required = True
+
+	def get(self, request):
+		return self.respond('settings.html', request)
