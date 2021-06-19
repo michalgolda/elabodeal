8324dde2b7134b0fa9f718e0bb164c9c25e8e0e0
Michal001 <michalgolda@vp.pl>
2021-06-19 12:10:25 +0200

    feat: add custom response interceptor for axios instance

--- elabodeal/web/static/js/api.js
+++ elabodeal/web/static/js/api.js
@@ -1,12 +1,35 @@
 import axios from 'axios';
+import Alert from '@/alert';
 
 
-const apiClient = axios.create( {
+const apiClient = axios.create({
     xsrfCookieName: 'csrftoken',
     xsrfHeaderName: 'X-CSRFtoken',
     headers: {
     	'content-type': 'multipart/form-data'
     }
-} );
+});
+
+apiClient.interceptors.response.use(
+    function (res) { return res },
+    function (err) {
+        const errResponse = err.response;
+
+        if (errResponse) {
+            const errResponseStatus = errResponse.status;
+
+            const isUnexpectedErr = errResponseStatus === 500;
+
+            if (isUnexpectedErr)
+                Alert.error( 
+                    `Wystąpił niespowdziewany błąd.
+                    Po zlokalizowaniu usterki, 
+                    postaramy się ją jak najszybciej usunąć.` 
+                );
+
+            return Promise.reject(errResponse);
+        }
+    }
+);
 
 export default apiClient;
