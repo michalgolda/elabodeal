16ae12bad342df4a36641c1f0af2d37ba7d8a2f3
Michał Gołda <michalgolda@vp.pl>
2021-06-21 15:48:22 +0200

    chore: add friendly-errors-webpack-plugin to development environment

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,14 @@
 const webpackConfig = require( './webpack.config' );
 
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+
+
 webpackConfig.mode = 'development';
 webpackConfig.devtool = 'eval';
 webpackConfig.watch = true;
+webpackConfig.plugins = [
+	...webpackConfig.plugins,
+	new FriendlyErrorsWebpackPlugin()
+];
 
 module.exports = webpackConfig;
