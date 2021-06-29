654ad8e8307897293d57340a2355b561be0a9530
Michał Gołda <michalgolda@vp.pl>
2021-06-29 18:45:28 +0200

    feat: add changeFirstName, changeLastName, changeSwift, changeAccountNumber actions

--- elabodeal/web/static/js/pages/settings/store/modules/publisherSettings.js
+++ elabodeal/web/static/js/pages/settings/store/modules/publisherSettings.js
@@ -1,5 +1,136 @@
+import publisherService from '@/services/publisher';
+
+
 const publisherSettingsModule = {
-	namespaced: true
+	namespaced: true,
+	actions: {
+		changeFirstName (ctx, { first_name }) {
+			const data = new FormData();
+
+			data.append('first_name', first_name);
+
+			publisherService.updateSettings(data, {
+				successCallback: () => {
+					ctx.commit(
+						'updateData',
+						{
+							key: 'publisher',
+							fieldName: 'first_name',
+							fieldValue: first_name
+						},
+						{root: true}
+					);
+
+					ctx.commit(
+						'ui/hideSection',
+						null,
+						{root: true}
+					);
+				},
+				errorCallback: () => {
+					ctx.commit(
+						'ui/setSectionError',
+						'INVALID_FORM_DATA',
+						{root: true}
+					);
+				}
+			});
+		},
+		changeLastName (ctx, { last_name }) {
+			const data = new FormData();
+
+			data.append('last_name', last_name);
+
+			publisherService.updateSettings(data, {
+				successCallback: () => {
+					ctx.commit(
+						'updateData',
+						{
+							key: 'publisher',
+							fieldName: 'last_name',
+							fieldValue: last_name
+						},
+						{root: true}
+					);
+
+					ctx.commit(
+						'ui/hideSection',
+						null,
+						{root: true}
+					);
+				},
+				errorCallback: () => {
+					ctx.commit(
+						'ui/setSectionError',
+						'INVALID_FORM_DATA',
+						{root: true}
+					);
+				}
+			});
+		},
+		changeSwift (ctx, { swift }) {
+			const data = new FormData();
+
+			data.append('swift', swift);
+
+			publisherService.updateSettings(data, {
+				successCallback: () => {
+					ctx.commit(
+						'updateData',						{
+							key: 'publisher',
+							fieldName: 'swift',
+							fieldValue: swift
+						},
+						{root: true}
+					);
+
+					ctx.commit(
+						'ui/hideSection',
+						null,
+						{root: true}
+					);
+				},
+				errorCallback: () => {
+					ctx.commit(
+						'ui/setSectionError',
+						'INVALID_FORM_DATA',
+						{root: true}
+					);
+				}
+			});
+		},
+		changeAccountNumber (ctx, { account_number }) {
+			const data = new FormData();
+
+			data.append('account_number', account_number);
+
+			publisherService.updateSettings(data, {
+				successCallback: () => {
+					ctx.commit(
+						'updateData',						{
+							key: 'publisher',
+							fieldName: 'account_number',
+							fieldValue: account_number
+						},
+						{root: true}
+					);
+
+					ctx.commit(
+						'ui/hideSection',
+						null,
+						{root: true}
+					);
+				},
+				errorCallback: () => {
+					ctx.commit(
+						'ui/setSectionError',
+						'INVALID_FORM_DATA',
+						{root: true}
+					);
+				}
+			});
+		}
+	}
 };
 
 export default publisherSettingsModule;
