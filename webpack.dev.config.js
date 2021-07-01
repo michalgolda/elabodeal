da2be99a5b818619b4525d3bf6b6d97e6d207f57
Michal001 <michalgolda@vp.pl>
2021-07-01 22:33:35 +0200

    chore: add linter for developer environment

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -5,6 +5,17 @@ const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 
 webpackConfig.mode = 'development';
 webpackConfig.devtool = 'eval';
+
+webpackConfig.module.rules.push({
+	enforce: 'pre',
+	test: /\.(js|vue)$/,
+	exclude: /node_modules/,
+	loader: 'eslint-loader',
+	options: {
+		formatter: 'codeframe'
+	}
+});
+
 webpackConfig.watch = true;
 webpackConfig.watchOptions = {
 	ignored: ['/node_modules/']
