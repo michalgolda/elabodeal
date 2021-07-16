a1f6150b298312347586f35b8c349fc5bf81a27a
Michal001 <michalgolda@vp.pl>
2021-07-16 17:11:48 +0200

    fix: if any component has not mount element other elements is not loading

--- elabodeal/web/static/js/utils/vue.js
+++ elabodeal/web/static/js/utils/vue.js
@@ -8,30 +8,30 @@ export const mountPageComponents = (vueInstance, components) => {
 		const vueComponent = components[key];
 		const mountElement = document.getElementById(key);
 
-		if (!mountElement) return;
-
-		const vMountElement = mountElement.cloneNode(true);
-
-		vMountElement.innerHTML = '';
-
-		mount(vueComponent, {
-			app: vueInstance,
-			props: { ...mountElement.dataset },
-			element: vMountElement
-		});
-
-		let vMountElementChildNodes = vMountElement.childNodes;
-		const vMountElementHasFragment = vMountElementChildNodes.length === 1;
-
-		if (!vMountElementHasFragment) {
-			const fragment = document.createElement('div');
-
-			fragment.append(...vMountElementChildNodes);
+		if (mountElement) {
+			const vMountElement = mountElement.cloneNode(true);
 
 			vMountElement.innerHTML = '';
-			vMountElement.appendChild(fragment);
-		}
 
-		mountElement.replaceWith(vMountElementChildNodes[0]);
+			mount(vueComponent, {
+				app: vueInstance,
+				props: { ...mountElement.dataset },
+				element: vMountElement
+			});
+
+			let vMountElementChildNodes = vMountElement.childNodes;
+			const vMountElementHasFragment = vMountElementChildNodes.length === 1;
+
+			if (!vMountElementHasFragment) {
+				const fragment = document.createElement('div');
+
+				fragment.append(...vMountElementChildNodes);
+
+				vMountElement.innerHTML = '';
+				vMountElement.appendChild(fragment);
+			}
+
+			mountElement.replaceWith(vMountElementChildNodes[0]);
+		}
 	}
 };
