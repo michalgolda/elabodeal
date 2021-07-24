2e888c36964d30639c4861c002335b664ea550b2
Michal001 <michalgolda@vp.pl>
2021-07-24 14:38:38 +0200

    feat: add products_count property

--- elabodeal/models/cart.py
+++ elabodeal/models/cart.py
@@ -1,7 +1,14 @@
+import uuid
 from django.db import models
 
 
 class Cart(models.Model):
+	id = models.UUIDField(
+		primary_key=True,
+		default=uuid.uuid4,
+		editable=False
+	)
+
 	user = models.ForeignKey(
 		'elabodeal.User', 
 		on_delete=models.CASCADE
@@ -14,3 +21,7 @@ class Cart(models.Model):
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
+
+	@property
+	def products_count(self):
+		return self.products.count()
