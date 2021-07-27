37afcdd8fa1688b3b6878a63cbdcdbf717831376
Michal001 <michalgolda@vp.pl>
2021-07-27 17:09:14 +0200

    feat: add delete function to savedCartService

--- elabodeal/web/static/js/services/savedCart.js
+++ elabodeal/web/static/js/services/savedCart.js
@@ -5,9 +5,16 @@ const savedCartService = new Service('savedCart');
 
 
 savedCartService.registerFunction('share', {
-    url: '/api/carts/<savedCartId>/share/',
+    url: '/api/me/carts/<savedCartId>/share/',
     method: 'post',
     urlVariables: ['savedCartId']
 });
+
+savedCartService.registerFunction('delete', {
+    url: '/api/me/carts/<savedCartId>/',
+    method: 'delete',
+    urlVariables: ['savedCartId']
+});
+
 
 export default savedCartService;
