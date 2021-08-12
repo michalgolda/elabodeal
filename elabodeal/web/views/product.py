1d74876b3bc6942da31ab01baeddfce56d0077b0
Michal001 <michalgolda@vp.pl>
2021-08-12 16:16:58 +0200

    feat: when user is authenticated check whether he has a current product

--- elabodeal/web/views/product.py
+++ elabodeal/web/views/product.py
@@ -1,9 +1,17 @@
 from elabodeal.web.views.base import BaseView
-from elabodeal.models import Product
+from elabodeal.models import (
+	Product,
+	PurchasedProduct
+)
 
 
 class ProductView(BaseView):
-	def get_product_formats(self, product):
+	def get(self, request, id):
+		product = Product.objects.filter(id=id).first()
+
+		if not product:
+			return self.respond('product_404.html', request)
+
 		product_files = product.files.all()
 
 		product_formats = []
@@ -14,20 +22,16 @@ class ProductView(BaseView):
 			if file_type == 'ebook':
 				product_formats.append(file_extension)
 
-		return product_formats
+		user = request.user
+		user_has_product = False
 
-	def get_context(self, product):
-		return {
+		if user.is_authenticated:
+			user_has_product = PurchasedProduct.objects.filter(product=product).exists()		
+
+		context = {
 			'product': product,
-			'product_formats': self.get_product_formats(product)
+			'product_formats': product_formats,
+			'user_has_product': user_has_product
 		}
-
-	def get(self, request, id):
-		product = Product.objects.filter(id=id).first()
-
-		if not product:
-			return self.respond('product_404.html', request)
-
-		context = self.get_context(product)
 
 		return self.respond('product.html', request, context)
