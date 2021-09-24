aaae6752227de722f1cd4e4ec4ec0343fcf3bdd3
Michal001 <michalgolda@vp.pl>
2021-09-24 20:56:31 +0200

    feat: add InputErrorsWrapper component

---
+++ elabodeal/web/static/js/components/InputErrorsWrapper.vue
@@ -0,0 +1,31 @@
+<template>
+    <p 
+        class="form__input-error-msg"
+        v-for="error in errors"
+        :key="error"
+    >
+        {{ error }}
+    </p>
+    <slot :isErrors="isErrors" />
+</template>
+<script>
+import { toRefs, reactive } from 'vue'
+
+
+export default {
+    name: 'InputErrorsWrapper',
+    props: {
+        errors: {
+            type: Array,
+            default: () => []
+        }
+    },
+    setup (props) {
+        const { errors } = toRefs(props)
+
+        const state = reactive({ isErrors: errors.value.length > 0 })
+
+        return { ...toRefs(state) }
+    }
+}
+</script>
