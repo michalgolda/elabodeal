f17b1d4fb278026654a8d6c5a480600162dd4969
Michal001 <michalgolda@vp.pl>
2021-09-24 21:52:42 +0200

    feat: add InputErrorsWrapper, CharCounter to form inputs

--- elabodeal/web/static/js/pages/profile/components/EditModal.vue
+++ elabodeal/web/static/js/pages/profile/components/EditModal.vue
@@ -1,110 +1,101 @@
 <template>
     <Modal title="Edytuj profil">
         <form
-            @submit.prevent="updateProfile"
             class="form editForm"
+            @submit.prevent="updateProfile"
         >
             <div class="form__input-group">
                 <label>Zdjęcie profilowe</label>
-                <p 
-                    class="form__input-error-msg"
-                    v-for="error in avatarImgErrors"
-                    :key="error"
-                >
-                    {{ error }}
-                </p>
-                <ImageInput 
-                    @upload="handleUploadAvatarImg"
-                    @remove="handleRemoveUploadedAvatarImg"
-                    :uploaded-file-src="avatarImgSrc"
-                    class="editImageInput-avatar" 
-                />
+                <InputErrorsWrapper :errors="errors.avatarImg">
+                    <ImageInput 
+                        @upload="handleUploadAvatarImg"
+                        @remove="handleRemoveUploadedAvatarImg"
+                        :uploaded-file-src="avatarImgSrc"
+                        class="editImageInput-avatar" 
+                    />
+                </InputErrorsWrapper>
             </div>
             <div class="form__input-group">
                 <label>Baner</label>
-                <p 
-                    class="form__input-error-msg"
-                    v-for="error in bannerImgErrors"
-                    :key="error"
-                >
-                    {{ error }}
-                </p>
-                <ImageInput 
-                    @upload="handleUploadBannerImg"
-                    @remove="handleRemoveUploadedBannerImg"
-                    :uploaded-file-src="bannerImgSrc"
-                    class="editImageInput-banner"
-                />
+                <InputErrorsWrapper :errors="errors.bannerImg">
+                    <ImageInput
+                        @upload="handleUploadBannerImg"
+                        @remove="handleRemoveUploadedBannerImg"
+                        :uploaded-file-src="bannerImgSrc"
+                        class="editImageInput-banner"
+                    />
+                </InputErrorsWrapper>
             </div>
             <div class="form__input-group">
                 <label>Wyrożniony produkt</label>
-                <p 
-                    class="form__input-error-msg"
-                    v-for="error in bannerProductErrors"
-                    :key="error"
-                >
-                    {{ error }}
-                </p>
-                <select ref="bannerProductInputRef">
-                    <option 
-                        :selected="bannerProductInputValue ? false : true"
-                        value=""
-                    >
-                        Brak wyróżnienia
-                    </option>
-                    <option
-                        v-for="product in products"
-                        :key="product.id"
-                        :value="product.id"
-                        :selected="bannerProductInputValue === product.id ? true : false"
-                    >
-                        {{ product.title }}
-                    </option>
-                </select>
+                <InputErrorsWrapper :errors="errors.bannerProduct">
+                    <select v-model="bannerProductInputValue">
+                        <option 
+                            value=""
+                            :selected="bannerProductInputValue ? false : true"
+                        >
+                            Brak wyróżnienia
+                        </option>
+                        <option
+                            v-for="product in products"
+                            :key="product.id"
+                            :value="product.id"
+                            :selected="bannerProductInputValue === product.id ? true : false"
+                        >
+                            {{ product.title }}
+                        </option>
+                    </select>
+                </InputErrorsWrapper>
             </div>
             <div class="form__input-group">
                 <label>Tekst widoczny na banerze</label>
-                <p 
-                    class="form__input-error-msg"
-                    v-for="error in bannerTextErrors"
-                    :key="error"
-                >
-                    {{ error }}
-                </p>
-                <input
-                    type="text"
-                    ref="bannerTextInputRef"
-                    :value="bannerTextInputValue"
-                >
+                <InputErrorsWrapper :errors="errors.bannerText">
+                    <CharCounter
+                        v-slot="charCounter"
+                        :max-value="50"
+                        :initial-value="bannerTextInputValue.length"
+                    >
+                        <input
+                            type="text"
+                            :maxlength="50"
+                            @input="charCounter.update"
+                            v-model="bannerTextInputValue"
+                        >
+                    </CharCounter>
+                </InputErrorsWrapper>
             </div>
             <div class="form__input-group">
                 <label>Kim jesteś ?</label>
-                <p 
-                    class="form__input-error-msg"
-                    v-for="error in whoYouAreErrors"
-                    :key="error"
-                >
-                    {{ error }}
-                </p>
-                <input 
-                    type="text"
-                    ref="whoYouAreInputRef"
-                    :value="whoYouAreInputValue"
-                >
+                <InputErrorsWrapper :errors="errors.whoYouAre">
+                    <CharCounter
+                        v-slot="charCounter"
+                        :max-value="50"
+                        :initial-value="whoYouAreInputValue.length"
+                    >
+                        <input
+                            type="text"
+                            :maxlength="50"
+                            @input="charCounter.update"
+                            v-model="whoYouAreInputValue"
+                        >
+                    </CharCounter>
+                </InputErrorsWrapper>
             </div>
             <div class="form__input-group">
                 <label>Bio</label>
-                <p 
-                    class="form__input-error-msg"
-                    v-for="error in bioErrors"
-                    :key="error"
-                >
-                    {{ error }}
-                </p>
-                <textarea 
-                    ref="bioInputRef"
-                    :value="bioInputValue"
-                />
+                <InputErrorsWrapper :errors="errors.bio">
+                    <CharCounter
+                        v-slot="charCounter"
+                        :max-value="100"
+                        :initial-value="whoYouAreInputValue.length"
+                    >
+                        <textarea 
+                            :maxlength="100"
+                            v-model="bioInputValue" 
+                            @input="charCounter.update"
+                        />
+                    </CharCounter>
+                </InputErrorsWrapper>
             </div>
             <button
                 type="submit"
@@ -116,164 +107,106 @@
     </Modal>
 </template>
 <script>
-import { computed, ref } from 'vue'
 import { useStore } from 'vuex'
+import { computed, reactive, toRefs } from 'vue'
 import { actionsTypes } from '../store/actions'
 
-import Modal from '@/components/Modal'
 import ImageInput from './ImageInput'
+import Modal from '@/components/Modal'
+import CharCounter from '@/components/CharCounter'
+import InputErrorsWrapper from '@/components/InputErrorsWrapper'
 
 
 export default {
-    components: { Modal, ImageInput },
+    name: 'EditModal',
+    components: { 
+        Modal,
+        ImageInput,
+        CharCounter,
+        InputErrorsWrapper
+    },
     setup () {
         const store = useStore()
 
-        const bioInputValue = computed(() => {
-            return store.state.profile.bio
+        const state = reactive({
+            bioInputValue: store.state.profile.bio,
+            whoYouAreInputValue: store.state.profile.who_you_are,
+            bannerTextInputValue: store.state.profile.banner_text,
+            bannerProductInputValue: computed(() => {
+                const product = store.state.profile.banner_product
+
+                return product ? product.id : null
+            }),
+            products: computed(() => store.state.products),
+            avatarImgSrc: computed(() => {
+                const avatarImg = store.state.profile.avatar_img
+
+                return avatarImg ? avatarImg.path : null
+            }),
+            bannerImgSrc: computed(() => {
+                const bannerImg = store.state.profile.banner_img
+
+                return bannerImg ? bannerImg.path : null
+            }),
+            avatarImg: { file: null, updated: false },
+            bannerImg:  { file: null, updated: false },
+            errors: {
+                bio: computed(() => store.state.errors.bio),
+                avatarImg: computed(() => store.state.errors.avatar_img),
+                bannerImg: computed(() => store.state.errors.banner_img),
+                whoYouAre: computed(() => store.state.errors.who_you_are),
+                bannerText: computed(() => store.state.errors.banner_text),
+                bannerProduct: computed(() => store.state.errors.banner_product)
+            }
         })
-
-        const whoYouAreInputValue = computed(() => {
-            return store.state.profile.who_you_are
-        })
-
-        const bannerTextInputValue = computed(() => {
-            return store.state.profile.banner_text
-        })
-
-        const bannerProductInputValue = computed(() => {
-            const bannerProduct = store.state.profile.banner_product
-
-            return bannerProduct && bannerProduct.id
-        })
-
-        const products = computed(() => {
-            return store.state.products
-        })
-
-        const avatarImgSrc = computed(() => {
-            const avatarImg = store.state.profile.avatar_img
-
-            return avatarImg ? avatarImg.path : null
-        })
-
-        const bannerImgSrc = computed(() => {
-            const bannerImg = store.state.profile.banner_img
-
-            return bannerImg ? bannerImg.path : null
-        })
-
-        const avatarImgFile = ref(null)
-
-        const updatedAvatarImg = ref(false)
 
         const handleUploadAvatarImg = (file) => {
-            avatarImgFile.value = file
-            updatedAvatarImg.value = true
+            state.avatarImg.file = file
+            state.avatarImg.updated = true
         }
         
         const handleRemoveUploadedAvatarImg = () => {
-            avatarImgFile.value = null
-            updatedAvatarImg.value = true
+            state.avatarImg.file = null
+            state.avatarImg.updated = true
         }
 
-        const bannerImgFile = ref(null)
-
-        const updatedBannerImg = ref(false)
-
         const handleUploadBannerImg = (file) => {
-            bannerImgFile.value = file
-            updatedBannerImg.value = true
+            state.bannerImg.file = file
+            state.bannerImg.updated = true
         }
 
         const handleRemoveUploadedBannerImg = () => {
-            bannerImgFile.value = null
-            updatedBannerImg.value = true
+            state.bannerImg.file = null
+            state.bannerImg.updated = true
         }
         
-        const bioInputRef = ref(null)   
-        const whoYouAreInputRef = ref(null)
-        const bannerTextInputRef = ref(null)    
-        const bannerProductInputRef = ref(null)
-
         const updateProfile = () => {
-            updatedBannerImg.value && store.dispatch(
+            state.bannerImg.updated && store.dispatch(
                 actionsTypes.UPDATE_PROFILE_BANNER_IMG,
-                { bannerImg: bannerImgFile.value }
+                { bannerImg: state.bannerImg.file }
             )
 
-            bannerImgFile.value = null
-
-            updatedAvatarImg.value && store.dispatch(
+            state.avatarImg.updated && store.dispatch(
                 actionsTypes.UPDATE_PROFILE_AVATAR_IMG,
-                { avatarImg: avatarImgFile.value }
+                { avatarImg: state.avatarImg.file }
             )
-
-            avatarImgFile.value = null
-            
-            const bioInput = bioInputRef.value
-            const whoYouAreInput = whoYouAreInputRef.value
-            const bannerTextInput = bannerTextInputRef.value
-            const bannerProductInput = bannerProductInputRef.value
 
             store.dispatch(
                 actionsTypes.UPDATE_PROFILE,
                 {
-                    bannerProduct: bannerProductInput.value,
-                    bio: bioInput.value ? bioInput.value : null,
-                    whoYouAre: whoYouAreInput.value ? whoYouAreInput.value : null,
-                    bannerText: bannerTextInput.value ? bannerTextInput.value : null
+                    bannerProduct: state.bannerProductInputValue,
+                    bio: state.bioInputValue ? state.bioInputValue : null,
+                    whoYouAre: state.whoYouAreInputValue ? state.whoYouAreInputValue : null,
+                    bannerText: state.bannerTextInputValue ? state.bannerTextInputValue : null
                 }
             )
         }
 
-        const avatarImgErrors = computed(() => {
-            return store.state.errors.avatar_img
-        })
-
-        const bannerImgErrors = computed(() => {
-            return store.state.errors.banner_img
-        })
-
-        const bannerProductErrors = computed(() => {
-            return store.state.errors.banner_product
-        })
-
-        const bannerTextErrors = computed(() => {
-            return store.state.errors.banner_text
-        })
-
-        const whoYouAreErrors = computed(() => {
-            return store.state.errors.who_you_are
-        })
-
-        const bioErrors = computed(() => {
-            return store.state.errors.bio
-        })
-
-        return { 
-            products,
-            bioErrors,
-            bioInputRef,
-            avatarImgSrc,
-            bannerImgSrc,
-            bioInputValue,
+        return {
+            ...toRefs(state),
             updateProfile,
-            whoYouAreErrors,
-            avatarImgErrors,
-            bannerImgErrors,
-            updatedBannerImg,
-            updatedAvatarImg,
-            bannerTextErrors,
-            whoYouAreInputRef,
-            bannerTextInputRef,
-            bannerProductErrors,
-            whoYouAreInputValue,
-            bannerTextInputValue,
-            bannerProductInputRef,
             handleUploadAvatarImg,
             handleUploadBannerImg,
-            bannerProductInputValue,
             handleRemoveUploadedAvatarImg,
             handleRemoveUploadedBannerImg
         }
